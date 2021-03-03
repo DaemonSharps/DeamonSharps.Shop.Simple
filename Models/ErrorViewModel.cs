aae32dd0bf5f3d94a15a593dfdbabc275a3e648f
Денис Смирнов <smirnov.god@gmail.com>
2021-03-03 19:26:30 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeamonSharps.Shop.Simple.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
