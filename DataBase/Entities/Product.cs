7fd8d2714735da939c323d953360838300b50d7b
Денис Смирнов <smirnov.god@gmail.com>
2021-03-14 13:42:16 +0300

    Решил проверить работает ли Pipelines

--- DataBase/Entities/Product.cs
+++ DataBase/Entities/Product.cs
@@ -10,11 +10,11 @@ namespace DeamonSharps.Shop.Simple.DataBase.Entities
         public int Id { get; set; }
         public string Product_Name { get; set; }
         public decimal Product_Price { get; set; }
-        public string Product_About { get; set; }
-        public List<ProductCategory> ProductCategory { get; set; }
+        public string Product_About { get; se
+        public List<ProductCategory> ProductCat
         public Product()
         {
-            ProductCategory = new List<ProductCategory>();
+            ProductCategory = n
         }
     }
 }
