457e332b6b07e8564745ca5184905c06a5d61d55
Денис Смирнов <smirnov.god@gmail.com>
2021-08-09 22:32:41 +0300

    Тесты OrdersController

---
+++ UnitTests/Controllers/OrdersControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using DaemonSharps.Shop.UnitTests.Mocks;
+using DeamonSharps.Shop.Simple.Controllers;
+using DeamonSharps.Shop.Simple.DataBase.Entities;
+using DeamonSharps.Shop.Simple.Models;
+using DeamonSharps.Shop.Simple.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Controllers
+{
+    public class OrdersControllerTests
+    {
+        [Theory]
+        [InlineData(1, 14)]
+        [InlineData(5, 4)]
+        public async Task Index_ReturnCorrectView_And_Model(int page, int count)
+        {
+            //Arrange
+            var orderServiceMock = new OrderServiceMock();
+            var controller = new OrdersController(orderServiceMock.Object);
+            var expectedStatus = new OrderStatus_DB
+            {
+                Id = 1,
+                Name = OrderStatus.Created.ToString()
+            };
+
+            //Act
+            var result = await controller.Index(page);
+
+            //Assert
+            var view = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<OrderPageViewModel>(view.Model);
+            Assert.Equal(page, model.CurrentPage);
+            Assert.Equal(OrderServiceMock.PageCount, model.PageCount);
+            Assert.Equal(count, model.Orders.Count);
+            for (int i = 0; i < model.Orders.Count; i++)
+            {
+                var order = model.Orders[i];
+                Assert.Equal(expectedStatus.Name, order.Status);
+                Assert.Single(order.Products);
+            }
+        }
+    }
+}
---
+++ UnitTests/Mocks/OrderServiceMock.cs
@@ -0,0 +1,45 @@
+﻿using DeamonSharps.Shop.Simple.DataBase.Entities;
+using DeamonSharps.Shop.Simple.Services.Interfaces;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DaemonSharps.Shop.UnitTests.Mocks
+{
+    public class OrderServiceMock: Mock<IOrderService>
+    {
+        public static readonly int PageCount = 5;
+        public OrderServiceMock()
+        {
+            Set();
+        }
+
+        private void Set()
+        {
+            Setup(os => os.GetPageCountAsync()).Returns(() => Task.FromResult(PageCount));
+
+            Setup(os => os.GetOrdersByFilterAsync(It.Is<int>(p => p == 1)))
+                .Returns(async () => 
+                {
+                    var orders = new List<Order_DB>();
+                    for (int i = 0; i < 14; i++)
+                    {
+                        orders.Add(Order_DB.GetDefaultValue(i));
+                    }
+                    return orders;
+                } );
+
+            Setup(os => os.GetOrdersByFilterAsync(It.Is<int>(p => p == 5)))
+                .Returns(async () =>
+                {
+                    var orders = new List<Order_DB>();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        orders.Add(Order_DB.GetDefaultValue(i));
+                    }
+                    return orders;
+                });
+        }
+
+    }
+}
